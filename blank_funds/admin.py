08e63798588427522d952a42cc6d8cfc3959d827
Anshuman Swain <anshuman8swain@gmail.com>
2023-05-06 13:45:30 +0530

    env fixed

--- blank_funds/admin.py
+++ blank_funds/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment, User, Organisation, Social
+
+admin.site.register(User)
+admin.site.register(Organisation)
+admin.site.register(Payment)
+admin.site.register(Social)
